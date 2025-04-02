ef0a062691ef72d08bd0914c8f2bb231c03bf943
AlterShot <Vigglex@mail.ru>
2025-04-02 12:12:36 +0300

    Добавлен клик по кнопке

---
+++ authorize.py
@@ -0,0 +1,27 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+options = webdriver.ChromeOptions()
+options.add_experimental_option("detach", True)
+
+driver = webdriver.Chrome(options=options, service=ChromeService(ChromeDriverManager().install()))
+base_url: str = "https://www.saucedemo.com/"
+driver.get(base_url)
+driver.set_window_size(1920, 1080)
+
+user_name = driver.find_element(By.XPATH, "//*[@id='user-name']")
+user_name.send_keys("performance_glitch_user")
+
+password = driver.find_element(By.XPATH, "//*[@id='password']")
+password.send_keys("secret_sauce")
+
+login_button = driver.find_element(By.ID, "login-button")
+login_button.click()
+
+time.sleep(10)
+driver.close()
