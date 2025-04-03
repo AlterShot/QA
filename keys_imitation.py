4afe194c0f35174c5bf986d4a05c6207f673a0ca
AlterShot <Vigglex@mail.ru>
2025-04-03 14:46:42 +0300

    Обновлено

--- keys_imitation.py
+++ keys_imitation.py
@@ -14,9 +14,15 @@ base_url: str = "https://www.saucedemo.com/"
 driver.get(base_url)
 driver.set_window_size(1920, 1080)
 
+# Вводим неверные данные в поле логина
 user_name = driver.find_element(By.XPATH, "//*[@id='user-name']")
-user_name.send_keys("performance_glitch_user")
-print("Логин введен")
+user_name.send_keys("performance_glitch")
+print("Неверный логин введен")
+
+# Вводим неверные данные в поле пароля
+password = driver.find_element(By.XPATH, "//*[@id='password']")
+password.send_keys("secret")
+print("Неверный пароль введен")
 
 time.sleep(2)
 
@@ -25,18 +31,22 @@ user_name.send_keys(Keys.CONTROL + 'a')
 user_name.send_keys(Keys.BACKSPACE)
 print("Поле логина очищено")
 
-time.sleep(2)
-
-password = driver.find_element(By.XPATH, "//*[@id='password']")
-password.send_keys("secret")
-print("Пароль введен")
-
-time.sleep(2)
-
 #Выделяем все поле пароля и удаляем
 password.send_keys(Keys.CONTROL + 'a')
 password.send_keys(Keys.BACKSPACE)
 print("Поле пароля очищено")
+
+time.sleep(2)
+
+# Вводим верные данные в поле логина
+user_name = driver.find_element(By.XPATH, "//*[@id='user-name']")
+user_name.send_keys("performance_glitch_user")
+print("Верный логин введен")
+
+# Вводим верные данные в поле пароля
+password = driver.find_element(By.XPATH, "//*[@id='password']")
+password.send_keys("secret_sauce")
+print("Верный пароль введен")
 
 time.sleep(2)
 
