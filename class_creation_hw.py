b26be2733c2bd2504cf785144bc7ef43f79bf1d5
AlterShot <Vigglex@mail.ru>
2025-04-15 16:33:48 +0300

    Обновлено

--- class_creation_hw.py
+++ class_creation_hw.py
@@ -1,7 +1,6 @@
 # Импортируем библиотеки
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
-from selenium.webdriver.support.expected_conditions import element_to_be_clickable
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
@@ -11,18 +10,22 @@ from selenium.webdriver.support import expected_conditions as EC
 # Создаем класс
 class Testing():
     def __init__(self):
-        self.driver = webdriver.Chrome(options=self.get_options(), service=ChromeService(ChromeDriverManager().install()))
+        self.__options = self.__get_options()
+        self.driver = self.__init_driver()
         self.driver.set_window_size(1920, 1080)
         self.base_url = 'https://www.saucedemo.com/'
 
-    def get_options(self):
+    def __get_options(self):
         options = webdriver.ChromeOptions()
         options.add_experimental_option("detach", True)
         return options
+
+    def __init_driver(self):
+        return webdriver.Chrome(options=self.__options, service=ChromeService(ChromeDriverManager().install()))
 
     def open_browser(self):
         self.driver.get(self.base_url)
 
 
-begin_test = Testing() # Создаем экземпляр класса
+begin_test = Testing()  # Создаем экземпляр класса
 begin_test.open_browser() # Вызываем метод класса
